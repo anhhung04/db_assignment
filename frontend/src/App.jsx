dedd7a2ec5c1b7a2ca3c15a747f3194ebe6143d8
phdhng <hung.phamdh@hcmut.edu.vn>
2024-04-22 19:15:03 +0700

    [#1] Add LoginPage.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react"
+import LoginForm from "./pages/LoginPage/LoginPage"
 
 export default function App() {
   const [state, setState] = useState("error")
   useEffect(() => {
-    async function fetchData(){
+    async function fetchData() {
       const data = await fetch("/api/demo").then(res => res.json())
-      if(data?.data) setState(data.data.message)
+      if (data?.data) setState(data.data.message)
     }
     fetchData()
   }, [state])
   return (
-    <>
-      <h1>{state}</h1>
-    </>
+    <div>
+      <LoginPage />
+    </div>
   )
 }
