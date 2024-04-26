fe655429b95b9441bf49288e680454c7a92fe9aa
Hồ Nhật Trị <honhattri2004@gmail.com>
2024-04-26 23:24:05 +0700

    add procedures

--- backend/migrations/1714127866100_insert-procedures.sql
+++ backend/migrations/1714127866100_insert-procedures.sql
@@ -1,3 +1,11 @@
 -- Up Migration
+CREATE OR REPLACE PROCEDURE display_user_names()
+AS $$
+BEGIN
+    UPDATE users SET display_name = CONCAT(lname, ' ', fname)
+    WHERE display_name IS NULL OR display_name = '';
+END;
+$$ LANGUAGE plpgsql;
+--CALL display_user_names();
 
 -- Down Migration
