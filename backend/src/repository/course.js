62d2b42dba326f84b46e2d99e54b030206e0ecbf
Anh-Hung Hoang <86340766+anhhung04@users.noreply.github.com>
2024-05-06 21:40:40 +0700

    Frontend/fix-fe (#75)
    * fe: list courses
    
    * Merge branch 'main' of https://github.com/anhhung04/db_assignment into frontend/fix-fe
    
    * set cookie when login
    
    * login
    
    * fix login
    
    * Merge branch 'main' of https://github.com/anhhung04/db_assignment into frontend/fix-fe
    
    * fix be
    
    * Merge branch 'main' of https://github.com/anhhung04/db_assignment into frontend/fix-fe
    
    * own course
    
    * fe
    
    * Merge branch 'main' into frontend/fix-fe
    
    * fix list student courses

--- backend/src/repository/course.js
+++ backend/src/repository/course.js
@@ -88,8 +88,8 @@ class CourseRepo extends IRepo {
             const result = await this.exec({
                 query: `
                     SELECT c.*, j.current_price as buy_price, j.created_at as buy_at
-                    FROM students_join_course j
-                    JOIN courses c ON students_join_course.course_id = courses.course_id AND students_join_course.student_id = $1${courseId ? ` WHERE ${isSlug ? "course_slug" : "course_id"} = $2` : ""}
+                    FROM students_join_courses sjc
+                    JOIN courses c ON sjc.course_id = courses.course_id AND sjc.student_id = $1${courseId ? ` WHERE ${isSlug ? "course_slug" : "course_id"} = $2` : ""}
                     LIMIT $3 OFFSET $4;
                 `,
                 args: [studentId, courseId, limit, (page - 1) * limit]
